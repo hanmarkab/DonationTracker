6ae14f9c666bcd7bf06962f100e6116a00f59475
Markab Han <hanmarkab@gmail.com>
2018-09-24 10:36:05 -0700

    Login

---
+++ app/src/main/java/com/example/markabhan/donationtracker/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.markabhan.donationtracker;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button loginScreenButton = (Button) findViewById(R.id.go_to_login_button);
+        loginScreenButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+            }
+        });
+
+        Button registerScreenButton = (Button) findViewById(R.id.go_to_registration_button);
+        registerScreenButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, RegistrationActivity.class));
+            }
+        });
+    }
+}
